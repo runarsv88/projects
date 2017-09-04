51381202dbde2e1f416150c90480b820bc73f26c
Runar Sverrisson <runarsv88@gmail.com>
2017-09-04 11:25:27 +0200

    Got a rudimentary Black Jack game up and running. No money involved

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -5,12 +5,12 @@ class Deck(object):
 
     def cards_left(self):
         left = 0
-        for number in cards:
+        for number in self.cards:
             left += number
         return left
 
     def get_card(self):
-        while self.cards_left > 0:
+        while self.cards_left() > 0:
             card = randrange(0,13)
             if self.cards[card] == 0:
                 continue
@@ -40,8 +40,47 @@ class Hand(object):
             if value < 11:
                value += 10
         return value
+    
+    def draw_card(self, deck):
+        self.cards.append(deck.get_card())
 
-deck = Deck()
-my_hand = Hand(deck)
-print(my_hand.cards)
-print(my_hand.sum())
+    def printer(self, player):
+        print("{}: {} with {} points".format(player, self.cards, self.sum()))
+
+while True:
+    cont = input("Continue to play (y/n)")  
+    if cont == 'y':
+        print("++++++++++++++++++++++++++++")
+    else:
+        break
+    deck = Deck()
+    my_hand = Hand(deck)
+    dealer_hand = Hand(deck)
+    my_hand.printer("My hand")
+    dealer_hand.printer("Dealer")
+    while my_hand.sum() < 22:
+        get_card = input("Get another card (y/n)?")
+        if get_card == 'y':
+            my_hand.draw_card(deck)
+            my_hand.printer("My hand")
+        else:
+            break
+    else: 
+        print("Busted! You have {}".format(my_hand.sum()))
+        print("House wins")
+        continue
+    while dealer_hand.sum() < 21 and dealer_hand.sum() <  my_hand.sum() and my_hand.sum() < 22:
+        dealer_hand.draw_card(deck)
+        dealer_hand.printer("Dealer")
+    if dealer_hand.sum() > 21:
+        print("House is bust! You Win!")
+    elif dealer_hand.sum() > my_hand.sum():
+        print("You lose!")
+    elif dealer_hand.sum() == my_hand.sum():
+        print("Draw, no winner")
+
+
+
+
+
+
