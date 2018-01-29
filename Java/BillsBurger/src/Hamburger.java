818a4beb0a720303f383247da43e748366110b4d
Sverrisson <rsverrisson@deloitte.dk>
2018-01-29 15:29:59 +0100

    Changed type of Additions to HashSet instead of array

--- Java/BillsBurger/src/Hamburger.java
+++ Java/BillsBurger/src/Hamburger.java
@@ -1,48 +1,52 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 /**
  * Created by rsverrisson on 29-01-2018.
  */
 public class Hamburger {
-    BreadType breadType;
-    MeatDoness meat;
-    Additions[] additions = {};
-    int numberOfAdds = 4;
-    int price;
+    private BreadType breadType;
+    private MeatDoness meat;
+    private HashSet<Additions> additions = new HashSet<>();
+    protected int numberOfAdds = 4;
+    protected int price;
 
-    Hamburger(BreadType breadType, MeatDoness meat, int price) {
+    Hamburger(BreadType breadType, MeatDoness meat) {
         this.breadType = breadType;
         this. meat = meat;
-        this.price = price;
+        this.price = 25;
     }
 
     public void printBurger(){
         System.out.println("Bread: "+ this.breadType);
         System.out.println("Meat doness: "+ this.meat);
-        System.out.println("Additions: "+ Arrays.toString(this.additions));
-        System.out.println("Total price: "+ this.price);
+        System.out.println("Base price: " + this.price);
+        System.out.println("Additions: ");
+        for (Additions add : this.additions) {
+            System.out.println(add + " price " + add.getPrice());
+        }
+        System.out.println("Total price: "+ this.burgerPrice());
     }
 
     public void addAdditions(Additions... adds) {
-        System.out.println("OMG!!!");
-        if (numberOfAdds >= additions.length + adds.length) {
-            Additions[] allAdds = new Additions[adds.length + this.additions.length];
-            Additions[][] arrs = {this.additions, adds};
-            System.out.println("Okk");
-            int counter  = 0;
-            for (Additions[] arr : arrs){
-                System.out.println(counter);
-                for (Additions add : arr) {
-                    System.out.println(add);
-                    allAdds[counter++] = add;
-                }
-            }
-            this.additions = allAdds;
+        if (numberOfAdds >= additions.size() + adds.length) {
+            additions.addAll(Arrays.asList(adds));
+        }
+        else {
+            throw new RuntimeException("Can't add all of this stuff to the burger!");
         }
     }
-   public enum BreadType {
+
+    public int burgerPrice() {
+        int totalPrice = this.price;
+        for (Additions add : this.additions) {
+            totalPrice += add.getPrice();
+        }
+        return totalPrice;
+    }
+    public enum BreadType {
        WHITE,
        BROWN_RYE,
        MAIZE;
@@ -59,6 +63,9 @@ public class Hamburger {
        Additions(int price){
            this.price = price;
        }
+       public int getPrice() {
+           return this.price;
+       }
    }
    public enum MeatDoness {
        NONE,
@@ -66,4 +73,19 @@ public class Hamburger {
        MEDIUM,
        WELL_DONE;
    }
+}
+
+class HealthyBurger extends Hamburger{
+   HealthyBurger(MeatDoness meat) {
+       super(BreadType.BROWN_RYE, meat);
+       this.numberOfAdds = 6;
+   }
+}
+
+class DeluxeBurger extends Hamburger {
+    DeluxeBurger(BreadType breadType, MeatDoness meatDoness) {
+        super(breadType, meatDoness);
+        numberOfAdds = 2;
+        addAdditions(Additions.FRIES, Additions.SODA);
+    }
 }
--- Java/BillsBurger/src/Main.java
+++ Java/BillsBurger/src/Main.java
@@ -6,11 +6,14 @@ public class Main {
         System.out.println("Let's make some burgeeeeeeeers!");
         Hamburger burger = new Hamburger(
                 Hamburger.BreadType.BROWN_RYE,
-                Hamburger.MeatDoness.MEDIUM,
-                10
+                Hamburger.MeatDoness.MEDIUM
         );
         burger.addAdditions(Hamburger.Additions.CARROT, Hamburger.Additions.FRIES);
         burger.addAdditions(Hamburger.Additions.LETTUCE);
+        burger.addAdditions(Hamburger.Additions.ONION);
         burger.printBurger();
+
+        DeluxeBurger delux = new DeluxeBurger(Hamburger.BreadType.BROWN_RYE, Hamburger.MeatDoness.MEDIUM);
+        delux.printBurger();
     }
 }
