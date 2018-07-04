2d41d40460ff7a8dbb4a4d538b575171011691e8
Sverrisson <rsverrisson@deloitte.dk>
2018-07-04 10:51:01 +0200

    Add playground java project to repo

---
+++ Java/Playground/src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] argv) {
+        System.out.println("Hello mi amigo");
+        String numbers = "65069058555305";
+        System.out.println(numbers.matches("\\d*"));
+    }
+}
+
