d4c72ad7c938ecca49df5468e82f4af80588fdba
Sverrisson <rsverrisson@deloitte.dk>
2018-07-10 16:02:08 +0200

    Add ProjectX and gitignore

---
+++ Java/ProjectX/src/com/runar/x/Main.java
@@ -0,0 +1,11 @@
+package com.runar.x;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        new X(x.nextInt()).x();
+    }
+}
---
+++ Java/ProjectX/src/com/runar/x/X.java
@@ -0,0 +1,17 @@
+package com.runar.x;
+
+/**
+ * Created by rsverrisson on 09-07-2018.
+ */
+public class X {
+    int x;
+
+    X(int x) {
+        this.x = x;
+    }
+    public void x() {
+        for (int x=0; x<=12; x++) {
+            System.out.println(x*this.x);
+        }
+    }
+}
